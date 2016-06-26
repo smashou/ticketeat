6044907b17543bc45a5861cb46dc902d7b57db97
Gache <vincent.gache@integral-service.fr>
2016-06-26 18:28:27 +0200

    Add home login templates

---
+++ src/UserBundle/Controller/SecurityController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use FOS\UserBundle\Controller\SecurityController as BaseController;
+
+class SecurityController extends BaseController
+{
+
+}
